e327a26b74adf8f58c3e44afbfcff9c7891cff7f
vinney cavallo <vcavallo@gmail.com>
2022-02-02 07:33:49 -0500

    Center mesg

--- interface/src/components/Mesg.vue
+++ interface/src/components/Mesg.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="mesg" class="fixed w-full px-4 top-4">
     <div @click="clearMesg" class="flex justify-between py-2 text-center shadow-lg alert rounded-md">
-      <div class="pl-2">
+      <div class="flex-grow pl-2 text-center">
         {{ mesg }}
       </div>
       <button @click="clearMesg" class="pr-2">
